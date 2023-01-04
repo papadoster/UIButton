a0179fa7022d2f0713e3a829c8e1fac91cc5e23c
papadoster <papadoster13@gmail.com>
2023-01-04 23:28:28 +0100

    1

--- UIButton/ViewController.swift
+++ UIButton/ViewController.swift
@@ -9,11 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var myButton = UIButton()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        myButton = UIButton(type: .roundedRect)
+        myButton.frame = CGRect(x: 50, y: 200, width: 100, height: 40)
+        myButton.setTitle("Press me", for: .normal)
+        myButton.setTitle("I'm Pressed", for: .highlighted)
+        myButton.tintColor = .black
+        myButton.backgroundColor = .cyan
+        myButton.addTarget(self, action: #selector(buttonIsPressed), for: .touchDown)
+        myButton.addTarget(self, action: #selector(buttonIsTapped), for: .touchUpInside)
+        view.addSubview(myButton)
+        
+        
     }
-
-
+    
+    @objc func buttonIsPressed(sender: UIButton) {
+        print("Button is pressed")
+    }
+    
+    @objc func buttonIsTapped(sender: UIButton) {
+        print("Button is tapped")
+    }
+    
 }
 
